6cc96beca6fe6a49a19a6f322915cfda967a2f42
@beimingyouxue <leiyingjia520@foxmail.com>
2021-11-08 09:54:09 +0800

    报错解决

--- vue-manage-system/src/views/statistical/NewStatistics.vue
+++ vue-manage-system/src/views/statistical/NewStatistics.vue
@@ -2,13 +2,13 @@
 	<el-tabs v-model="activeName">
 		<el-tab-pane label="图表" name="first">
 			<div class="div1">
-				<div class="div2">
-					
-				</div>
-				<div class="div2">
+				<div class="div2" style="background:linear-gradient(90deg, rgb(198, 119, 232), rgb(108, 133, 233)">
 
 				</div>
-				<div class="div2">
+				<div class="div2" style="background:linear-gradient(90deg, rgb(247, 202, 108), rgb(254, 150, 62))">
+
+				</div>
+				<div class="div2" style="background:linear-gradient(90deg, rgb(70, 193, 242), rgb(99, 151, 210))">
 
 				</div>
 			</div>
@@ -40,11 +40,12 @@
 		width: 100%;
 		height: 200px;
 	}
+
 	.div2 {
 		margin-right: 20px;
 		display: inline-block;
 		positiom: absolute;
-		width: 30%;
+		width: 33%;
 		height: 200px;
 	}
 </style>
